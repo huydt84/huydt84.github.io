30c450e4c17d4dbeb9962d5b36614beff42e4a7e
huydt84 <viethuy061002@gmail.com>
2026-06-28 21:06:38 +0900

    refactor: integrate KaTeX and remark-math for enhanced math rendering in blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,24 @@
 import { defineConfig } from "astro/config";
+import { unified } from "@astrojs/markdown-remark";
 
 import tailwindcss from "@tailwindcss/vite";
 
 import mdx from "@astrojs/mdx";
+import rehypeKatex from "rehype-katex";
 
 import react from "@astrojs/react";
 
 import sitemap from "@astrojs/sitemap";
+import remarkMath from "remark-math";
 
 export default defineConfig({
   site: "https://huydt84.github.io",
+  markdown: {
+    processor: unified({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+  },
   vite: {
     plugins: [tailwindcss()],
   },
